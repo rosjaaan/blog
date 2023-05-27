8db46cd0ebb309551f68242c50d09bccdd3e027e
rosjaaan <127468619+rosjaaan@users.noreply.github.com>
2023-05-27 12:06:29 +0530

    Add files via upload

---
+++ blog.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def svelte2():
+    return render_template('svelte2.html')
+
+@app.route('/day1')
+def day1():
+    return render_template('day1.html')
+
+@app.route('/day2')
+def day2():
+    return render_template('day2.html')
+
+@app.route('/day3')
+def day3():
+    return render_template('day3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
